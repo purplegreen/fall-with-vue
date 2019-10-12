87876ac15f9fdfa533353ece960cc700053b18a5
Admin <user@MacBook-Air.local>
2019-10-12 21:56:27 +0200

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.svg" />
+      <div class= "big-frame">
+          <Shot msg="so nice" />
+          <Shot msg="so nice" />
+          <Shot msg="so nice" />
+          <Shot msg="so nice" />
+          <Shot msg="so nice" />
+      </div>
+  </div>
+</template>
+
+<script>
+import Shot from "./components/Shot.vue";
+
+export default {
+  name: "app",
+  components: {
+    Shot
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.big-frame {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: flex-start;
+}
+</style>
---
+++ src/components/Shot.vue
@@ -0,0 +1,66 @@
+    <template>
+  <div class="thisbox">
+    <h1>{{ msg }}</h1>
+    <p>
+      more info at:
+      <a href="https://dehlix.com" target="_blank" rel="noopener"
+        >dehlix.com</a>.
+    </p>    
+    <h3>Eco</h3>
+    <ul>
+      <li>
+       one
+      </li>
+      <li>
+       two
+      </li>
+      <li>
+        three
+      </li>
+      <li>
+       four
+      </li>
+      <li>
+        five
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Shot",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.thisbox {
+  display: flex;
+  flex-direction: column;
+
+  width: 38vw;
+  background-color: beige;
+  padding: 12px;
+  margin-top: 10px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  font-size: 0.9em;
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
